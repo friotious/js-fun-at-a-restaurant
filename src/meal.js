0b7e666381e5d8a42fd6942600a7fb10f3802881
Rio Foster <friotious@gmail.com>
2021-12-03 20:31:53 -0800

    Add complete meal-test

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,49 @@
-function nameMenuItem() {
+function nameMenuItem(item) {
+return `Delicious ${item}`
+}
 
+function createMenuItem(name, price, type) {
+  var menuItem = {
+     name: name,
+     price: price,
+     type: type,
+  }
+  return menuItem;
+}
+
+function addIngredients(topping1, ingredients) {
+  for (var i = 0; i < ingredients.length; i++ ) {
+   if (ingredients[i] == topping1) {
+   return
+    }
+  }
+return ingredients.push(topping1)
+
+}
+
+function formatPrice(price) {
+return `$${price}`
+}
+
+function decreasePrice(price) {
+  return  (.9 * price)
+}
+
+function createRecipe(title, ingredients, menuItemType) {
+  var recipe = {
+  title: title,
+  ingredients: ingredients,
+  type: menuItemType,
+  }
+  return recipe
 }
 
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
