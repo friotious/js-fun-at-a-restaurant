f2dc8ad1ace6b8d8c210802c6c8fb6fbabc551f5
Rio Foster <friotious@gmail.com>
2021-12-05 20:56:47 -0800

    Add notes at line 16

--- src/restaurant.js
+++ src/restaurant.js
@@ -10,6 +10,16 @@ function createRestaurant(nameOfRestaurant) {
   return restaurant
 }
 
+//
+//
+//
+// I had a ton of help with the following code, and while I understand whats going
+// on, I'm not 100% and it would be difficult for me to repeat it, but I was trying to get a grasp
+// of the last concepts!
+//
+//
+//
+
 function addMenuItem(typeOfRest, dish) {
   //typeOfRest.menus[dish.type].push(dish)
   //for (var i = 0; i < typeOfRest.menus.lunch.length; i++)
@@ -23,11 +33,12 @@ function addMenuItem(typeOfRest, dish) {
   return typeOfRest
 }
 
+
 function removeMenuItem(typeOfRest, dish, type) {
-  for ( var i = 0; i < typeOfRest.menus[type]; i++)
-   if (typeOfRest.menus[type[i]] == dish) {
+  for ( var i = 0; i < typeOfRest.menus[type].length; i++)
+   if (typeOfRest.menus[type][i].name == dish) {
      return `No one is eating our ${dish} - it has been removed from the ${type} menu!`;
-     typeOfRest.menus[type[i].splice(i, 1)]
+     typeOfRest.menus[type].splice(i, 1)
    }
 }
 
